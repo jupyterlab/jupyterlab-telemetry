272c2fe960c46bb6818604c9adaa19d43831dc90
Ian Rose <ian.r.rose@gmail.com>
2018-01-25 12:03:28 -0800

    Save the log every two minutes.

--- src/index.ts
+++ src/index.ts
@@ -25,20 +25,31 @@ const extension: JupyterLabPlugin<void> = {
   activate: (app: JupyterLab) => {
     const { commands } = app;
     const handler = new TelemetryHandler();
-    const sessionLog = {
-      id: uuid(),
-      commands: [] as Telemetry.ICommandExecuted[]
-    };
+    // Make a uuid for this session, which will be its
+    // key in the session data.
+    const id = uuid();
+    // A log of executed commands.
+    const commandLog: Telemetry.ICommandExecuted[] = [];
 
+    // When a command is executed, store it in the log.
     commands.commandExecuted.connect((registry, command) => {
       const date = new Date();
-      sessionLog.commands.push({
+      commandLog.push({
         id: command.id,
         args: command.args,
         date: date.toJSON(),
       });
-      handler.save(sessionLog);
     });
+
+    const saveLog = () => {
+      const outgoing = commandLog.splice(0);
+      handler.save({ id, commands: outgoing }).catch(() => {
+        // If the save fails, put the outgoing list back in the log.
+        commandLog.unshift(...outgoing);
+      });
+    };
+    // Save the log to the server every two minutes.
+    setInterval(saveLog, 120 * 1000);
   }
 };
 
