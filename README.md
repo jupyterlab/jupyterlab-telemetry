e7fcb0507be32a2784cae03737dd3e7c82446738
Jaipreet Singh <jaipreet@amazon.com>
2019-09-14 18:47:20 -0700

    Initial cut of JupyterLab EventLog
    * Provides a way to subscribe handlers to the EventLog. The fan-out of events to each configured "handler" is done with PhoshporJS signals under the hood.
    * The `recordEvent` method allows publishers to send custom events to each configured handler
    * The EventLog can also subscribe to the JupyterLab CommandRegistry automatically and send JupyterLab commands to each handler.
    
    Other features which will be added incrementally
    - Schema validation and Settings Registry integration

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 A JupyterLab extension for logging and telemetry of usage data
 
-
 ## Prerequisites
 
-* JupyterLab
+* JupyterLab 1.0+
 
 ## Installation
 
@@ -13,20 +12,79 @@ A JupyterLab extension for logging and telemetry of usage data
 jupyter labextension install jupyterlab-telemetry
 ```
 
+## Usage
+
+Define handlers that can receive events from the `EventLog`
+
+```typescript
+import { EventLog } from "@jupyterlab/jupyterlab-telemetry";
+
+function consoleHandler(el: EventLog, events: EventLog.RecordedEvent[]) {
+  console.log(`[Handler1] Received events ${JSON.stringify(events)}`)
+}
+
+function consoleHandler2(el: EventLog, events: EventLog.RecordedEvent[]) {
+  console.log(`[Handler2] Received events ${JSON.stringify(events)}`)
+}
+```
+
+Create an instance of the EventLog and configure the handler and other options from `EventLog.IOptions`
+
+```typescript
+
+const el = new EventLog({
+  handlers: [consoleHandler, consoleHandler2],
+  allowedSchemas: ['org.jupyter.foo', 'org.jupyterlab.commands.docmanager:open'],
+  commandRegistry: app.commands,
+  commandEmitIntervalSeconds: 2
+});
+```
+
+Send custom events via the `recordEvents` interface. If the `commandRegistry` instance was passed, then the `EventLog`  will subscribe to commands executed in the JuptyerLab application and send the whitelisted ones to each configured handler.
+
+```typescript
+el.recordEvent({
+  schema: 'org.jupyter.foo',
+  version: 1,
+  body: {
+    'foo': 'bar'
+  }
+});
+
+```
+
+Dispose the event log after use
+
+```typescript
+el.dispose();
+```
+
 ## Development
 
-For a development install (requires npm version 4 or later), do the following in the repository directory:
+For a development install, do the following in the repository directory:
 
 ```bash
-npm install
-npm run build
-jupyter labextension link .
+yarn
+yarn build
+jupyter labextension install .
 ```
 
 To rebuild the package and the JupyterLab app:
 
 ```bash
-npm run build
+yarn build
 jupyter lab build
 ```
 
+To auto-build the package and JupyterLab on any change:
+
+```bash
+
+# In one terminal
+yarn watch
+
+# In second terminal
+
+jupyter lab --watch
+
+```
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,8 @@
 // Distributed under the terms of the Modified BSD License.
 
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
-import { Dialog, showDialog } from '@jupyterlab/apputils';
-import { UUID } from '@phosphor/coreutils';
-import { Widget } from '@phosphor/widgets';
+
 import '../style/index.css';
-import { Telemetry, TelemetryHandler } from './handler';
 
 /**
  * Initialization data for the jupyterlab-telemetry extension.
@@ -15,67 +12,9 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-telemetry',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    const { commands } = app;
-    const handler = new TelemetryHandler();
-    const id = UUID.uuid4()
-    // A log of executed commands.
-    const commandLog: Telemetry.ICommandExecuted[] = [];
-
-    app.restored.then(() => {
-      // Create the disclaimer dialog
-      const title = 'JupyterLab UX Survey'
-      const message = 'Are you willing to participate in a ' +
-                      'JupyterLab user-experience survey? ' +
-                      'If you agree, we will log:';
-      const message2 = 'We will NOT track:';
-      const dos = 'The menu items you use, the command palette commands you run, the keyboard shortcuts you invoke, the keyboard shortcuts you invoke, the filebrowser operations you use.'
-      const donts = 'The contents of any notebooks or other files, the code you run, or the commands you give in terminals.'
-      const body = `${message} ${dos} ${message2} ${donts}`
-
-      showDialog({
-        title,
-        body,
-        buttons: [
-          Dialog.cancelButton({ label: 'NO WAY!' }),
-          Dialog.okButton({ label: 'SURE!' }),
-        ]
-      }).then(result => {
-        if (result.button.accept) {
-          // Add a telemetry icon to the top bar.
-          // We do it after the app has been restored to place it
-          // at the right.
-          const widget = new Widget();
-          widget.addClass('jp-telemetry-icon');
-          widget.id = 'telemetry:icon';
-          widget.node.title = 'Telemetry data is being collected';
-          app.shell.add(widget, 'top');
-
-          // When a command is executed, store it in the log.
-          commands.commandExecuted.connect((registry, command) => {
-            const date = new Date();
-            commandLog.push({
-              id: command.id,
-              args: command.args,
-              date: date.toJSON(),
-            });
-          });
-
-          const saveLog = () => {
-            if (commandLog.length === 0) {
-              return;
-            }
-            const outgoing = commandLog.splice(0);
-            handler.save({ id, commands: outgoing }).catch(() => {
-              // If the save fails, put the outgoing list back in the log.
-              commandLog.unshift(...outgoing);
-            });
-          };
-          // Save the log to the server every two minutes.
-          setInterval(saveLog, 120 * 1000);
-        }
-      });
-    });
+    console.log('Telemetry extension activated');
   }
 };
 
 export default extension;
+export * from './eventlog'
