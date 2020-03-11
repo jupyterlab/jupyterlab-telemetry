d764bb5b4d1276130dd7b537e11c2a43c067c0c8
Nick Morris <npmorris@amazon.com>
2020-03-11 14:57:01 -0700

    Remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,4 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
-
-import '../style/index.css';
-
-/**
- * Initialization data for the jupyterlab-telemetry extension.
- */
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-telemetry',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('Telemetry extension activated');
-  }
-};
-
-export default extension;
 export * from './eventlog'
