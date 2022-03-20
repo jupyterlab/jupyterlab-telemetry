4bfbe8e0085bfe34871474497703d845e98dab3b
Piyush Jain <pjain@Piyushs-MacBook-Air.local>
2022-03-19 17:39:50 -0700

    Fix for build workflow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
 export * from './eventlog';
+export * from './handler';
+
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@jupyterlab/jupyterlab-telemetry',
+  autoStart: false,
+  activate: async (app: JupyterFrontEnd) => {
+    // Emplty plugin to satisfy build
+  }
+};
+
+export default plugin;
